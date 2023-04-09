557bd506dd321e0e2aa753472cb5c563461ab772
Akash kawale <67337118+akashkawale2@users.noreply.github.com>
2023-04-09 22:21:10 +0000

    added dynamic data and route

--- app.py
+++ app.py
@@ -1,11 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'company': 'Amazon',
+    'location': 'Bengluru, India',
+    'salary': 'Rs. 7,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'company': 'TCS',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 9,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Full Stack Engineer',
+    'company': 'Wipro',
+    'location': 'Mumbai, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'company': 'Infosys',
+    'location': 'Mumbai, India',
+    'salary': 'Rs. 12,00,000'
+  },
+  {
+    'id': 5,
+    'title': 'Frontend Engineer',
+    'company': 'Fyle',
+    'location': 'Remote',
+    'salary': 'Rs. 8,00,000'
+  },
+]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
