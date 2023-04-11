1a6c0a1f6ec68b8b779fd51b84e63a6f925fe630
akashkawale2 <67337118+akashkawale2@users.noreply.github.com>
2023-04-11 12:23:51 +0000

    Added dynamic pages for each job

--- database.py
+++ database.py
@@ -19,3 +19,22 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append((row))
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"SELECT * FROM jobs WHERE id = {id}"))
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._asdict()
+
+
+# def load_job_from_db(id): with engine.connect() as conn: result = conn.execute( text(f"SELECT * FROM jobs WHERE id = {id}") )
+
+# rows = result.all()
+# if len(rows) == 0:
+#   return None
+# else:
+#   return rows[0]._asdict()
