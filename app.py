8138e5003b3c8fa4400e78d77709b8ef6c125df2
akashkawale2 <67337118+akashkawale2@users.noreply.github.com>
2023-04-11 06:37:37 +0000

    Added connection to database

--- app.py
+++ app.py
@@ -1,52 +1,18 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'company': 'Amazon',
-    'location': 'Bengluru, India',
-    'salary': 'Rs. 7,00,000'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'company': 'TCS',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 9,00,000'
-  },
-  {
-    'id': 3,
-    'title': 'Full Stack Engineer',
-    'company': 'Wipro',
-    'location': 'Mumbai, India',
-    'salary': 'Rs. 10,00,000'
-  },
-  {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'company': 'Infosys',
-    'location': 'Mumbai, India',
-    'salary': 'Rs. 12,00,000'
-  },
-  {
-    'id': 5,
-    'title': 'Frontend Engineer',
-    'company': 'Fyle',
-    'location': 'Remote',
-    'salary': 'Rs. 8,00,000'
-  },
-]
 
 @app.route("/")
-def hello_world():
-  return render_template('home.html', jobs=JOBS)
+def hello_worksparrow():
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
